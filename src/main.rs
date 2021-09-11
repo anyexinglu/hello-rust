288c721e8c6afa7120d2c8c5633964fdd24dd0b0
anyexinglu <1171009543@qq.com>
2021-09-11 17:27:01 +0800

    猜数字游戏

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
-use ferris_says::say; // from the previous step
-use std::io::{stdout, BufWriter};
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
+    // --snip--
+    println!("Guess the number!");
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+    
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+    
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
